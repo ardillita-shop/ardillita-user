427acbbf20605707c85d23d25c383c01ae7113d7
Jan Steinbichler <jansteinbichler@wp.pl>
2023-08-02 21:11:57 +0200

    Spring Initializr

---
+++ src/main/java/com/ardillita/user/ArdillitaUserApplication.java
@@ -0,0 +1,13 @@
+package com.ardillita.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArdillitaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArdillitaUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ardillita/user/ArdillitaUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardillita.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArdillitaUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
